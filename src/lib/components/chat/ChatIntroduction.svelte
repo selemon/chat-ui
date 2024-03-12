d7615c542d392cc2da1ea86daf16e37a053f907a
Selemon Yitbarek <selemon3yitbarek@hotmail.com>
2024-03-12 14:30:26 +1300

    Update ChatIntroduction.svelte

--- src/lib/components/chat/ChatIntroduction.svelte
+++ src/lib/components/chat/ChatIntroduction.svelte
@@ -45,42 +45,6 @@
 			</p>
 		</div>
 	</div>
-	<div class="lg:col-span-2 lg:pl-24">
-		{#each announcementBanners as banner}
-			<AnnouncementBanner classNames="mb-4" title={banner.title}>
-				<a
-					target="_blank"
-					href={banner.linkHref}
-					class="mr-2 flex items-center underline hover:no-underline">{banner.linkTitle}</a
-				>
-			</AnnouncementBanner>
-		{/each}
-		<div class="overflow-hidden rounded-xl border dark:border-gray-800">
-			<div class="flex p-3">
-				<div>
-					<div class="text-sm text-gray-600 dark:text-gray-400">Current Model</div>
-					<div class="flex items-center gap-1.5 font-semibold max-sm:text-smd">
-						{#if currentModel.logoUrl}
-							<img
-								class=" overflown aspect-square size-4 rounded border dark:border-gray-700"
-								src={currentModel.logoUrl}
-								alt=""
-							/>
-						{:else}
-							<div class="size-4 rounded border border-transparent bg-gray-300 dark:bg-gray-800" />
-						{/if}
-						{currentModel.displayName}
-					</div>
-				</div>
-				<a
-					href="{base}/settings/{currentModel.id}"
-					class="btn ml-auto flex h-7 w-7 self-start rounded-full bg-gray-100 p-1 text-xs hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:hover:bg-gray-600"
-					><IconGear /></a
-				>
-			</div>
-			<ModelCardMetadata variant="dark" model={currentModel} />
-		</div>
-	</div>
 	{#if currentModelMetadata.promptExamples}
 		<div class="lg:col-span-3 lg:mt-6">
 			<p class="mb-3 text-gray-600 dark:text-gray-300">Examples</p>
